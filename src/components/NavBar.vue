e63f4a38227c385908a17d7bbfa564ca76ee0f0e
thebabush <1985669+thebabush@users.noreply.github.com>
2018-05-21 14:10:17 -0700

    Update username

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
     <a class="navbar-brand" href="#">GRUB Tunes</a>
     <ul class="nav navbar-nav pull-xs-right">
       <li class="nav-item active">
-        <a class="nav-link" href="https://github.com/kenoph/grub-tunes"><span class="fa fa-github"></span> GitHub</a>
+        <a class="nav-link" href="https://github.com/thebabush/grub-tunes"><span class="fa fa-github"></span> GitHub</a>
       </li>
     </ul>
   </nav>
